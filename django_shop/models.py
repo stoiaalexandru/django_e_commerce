96bc8ad3d4870fad3107ec382325f578adfb38e1
stoiaalexandru <63856047+stoiaalexandru@users.noreply.github.com>
2021-04-05 17:52:06 +0300

    OrderHistoryItem and ProductDetails added

--- django_shop/models.py
+++ django_shop/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django_users.models import CustomUser
-from enum import Enum
+from django.core.validators import MinValueValidator
+from django.db.migrations.operations import RunSQL
 # Create your models here.
 
 
@@ -46,14 +47,17 @@ class ShoppingCart(models.Model):
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     ordered = models.DateField()
-    shipped = models.DateField()
+    shipped = models.DateField(blank=True, null=True)
     ship_to = models.CharField(max_length=500)
     status = models.CharField(max_length=256,choices=OrderStatus.choices, default=OrderStatus.New)
 
+    def __str__(self):
+        return self.customer.get_full_name() + " order " + str(self.pk)
+
 
 class LineItem(models.Model):
-    cart = models.ForeignKey(ShoppingCart, on_delete=models.RESTRICT)
-    order = models.ForeignKey(Order, on_delete=models.RESTRICT)
+    cart = models.ForeignKey(ShoppingCart, on_delete=models.RESTRICT, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.RESTRICT, blank=True, null=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     price = models.FloatField()
@@ -74,3 +78,21 @@ class Key(models.Model):
 
     def __str__(self):
         return self.product.name + " key"
+
+
+class ProductDetail(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    price = models.FloatField(default=0, validators=([MinValueValidator(0)]))
+    quantity = models.IntegerField(
+        default=0,
+        validators=([MinValueValidator(0)]))
+
+    def __str__(self):
+        return self.product.name + " details"
+
+
+class OrderHistoryItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=256)
+    price = models.FloatField(default=0, validators=([MinValueValidator(0)]))
+    quantity = models.IntegerField(default=0, validators=([MinValueValidator(0)]))
