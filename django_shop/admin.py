0f41cf77a6663c7cbb5d1da36454d6fb04814b74
stoiaalexandru <63856047+stoiaalexandru@users.noreply.github.com>
2021-04-05 16:57:50 +0300

    models string methods

--- django_shop/admin.py
+++ django_shop/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+admin.site.register(models.Product)
+admin.site.register(models.Order)
+admin.site.register(models.Customer)
+admin.site.register(models.Key)
+admin.site.register(models.LineItem)
+admin.site.register(models.ShoppingCart)
+admin.site.register(models.Payment)
--- django_shop/models.py
+++ django_shop/models.py
@@ -14,19 +14,33 @@ class OrderStatus(models.TextChoices):
 
 class Customer(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     address = models.CharField(max_length=512)
     phone = models.CharField(max_length=15)
     billing_address = models.CharField(max_length=256)
+
+    def get_full_name(self):
+        return self.first_name + " " + self.last_name
+
+    def __str__(self):
+        return self.get_full_name()
 
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
     supplier = models.CharField(max_length=256)
 
+    def __str__(self):
+        return self.name
+
 
 class ShoppingCart(models.Model):
     customer = models.OneToOneField(Customer, on_delete=models.CASCADE)
     created = models.DateField()
+
+    def __str__(self):
+        return self.customer.get_full_name() + "'s Shopping Cart"
 
 
 class Order(models.Model):
@@ -52,3 +66,11 @@ class Payment(models.Model):
     total = models.FloatField()
     details = models.CharField(max_length=500)
 
+
+class Key(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    key = models.CharField(max_length=64)
+    is_used = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.product.name + " key"
