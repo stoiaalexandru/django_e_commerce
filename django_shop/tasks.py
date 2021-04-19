324b2faad884ba79a7428d2844ddbc1052cab69b
stoiaalexandru <63856047+stoiaalexandru@users.noreply.github.com>
2021-04-19 15:57:41 +0300

    OrderDetailView Done + order single mail test 3

--- django_shop/tasks.py
+++ django_shop/tasks.py
@@ -47,5 +47,5 @@ def send_order_history_single_email(template, order_pk):
 
         email = EmailMultiAlternatives(mail_subject, message, "no-reply@e-commerce.ro", to=[order.customer.user.email])
         email.attach_alternative(message, "text/html")
-        email.attach(order_file.name, order_file.read(), 'text/csv')
+        email.attach(order_file.name,'text/csv')
         email.send()
